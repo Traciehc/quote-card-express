2199b91b1dde224d149e113ba7928ab934059f7e
Tracie Childers <traciechilders01@gmail.com>
2025-02-12 20:53:46 -0500

    updated file

--- server.js
+++ server.js
@@ -5,13 +5,40 @@ const app = express();
 const path = require("path");
 const port = 1776;
 
+require("dotenv").config();
+const cors = require("cors");
+const corsOptions = {
+    origin : `http://localhost:1776`
+}
+app.use(cors(corsOptions))
+
 //Serves the front-end content in the public directory
 
 app.use("", express.static(path.join(__dirname, "./public")));
 
 app.use(express.json());
-
 app.use(express.urlencoded({extended:false}));
+
+async function getRandomPhoto() {
+    const url = `https://api.unsplash.com/photos/random/?client_id=${process.env.client_id}`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json()
+        const receivedPhoto = data.urls.regular
+        return receivedPhoto
+    }catch(error) {
+        console.log(error);
+    }
+}
+
+app.get("/api/getRandomImage", (request,response) => {
+    getRandomPhoto().then((returnedPhoto) => {
+         response.status(200).json({
+        status: 200,
+        data: process.env.client_id
+         })
+    })
+})
 
 //Serves the whole app
 
